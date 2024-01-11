7bcddd60b21a5e3d9fab2cb95ecfadfcb98d6244
konstantin370 <medvedev_konstantin2000@mail.ru>
2024-01-11 14:11:23 +0300

    lab1

--- task1/task.py
+++ task1/task.py
@@ -1,15 +1,43 @@
 import csv
+import json
+import argparse
+from jsonpath_ng import parse
 
-i = 5
-j = 3
 
-with open('file.csv', 'r', encoding='utf-8') as csvfile:
-    csvreader = csv.reader(csvfile)
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name_of_def', type=str, choices=['csv', 'json'], help='Input name of def (csv or json)')
+    parser.add_argument('value', nargs='+', type=str, help='Input row/column number or JSONPath example - $.str.str1')
+    args = parser.parse_args()
+    return args
 
-    for _ in range(i - 1):
-        next(csvreader)
-    
-    row = next(csvreader)
-    element = row[j - 1]
 
-    print(f'На позиции ({i}, {j}): {element}')
+def csv_reader(row, column):
+    filename = "example.csv"
+    f = open(filename, 'r')
+    csv_file = csv.reader(f)
+    csv_table = [[item for item in line] for line in csv_file]
+    return csv_table[row][column]
+
+
+def json_reader(jpath):
+    filename = 'example.json'
+    f = open(filename, 'r')
+    json_data = json.load(f)
+    jsonpath_expression = parse(jpath)
+    match = jsonpath_expression.find(json_data)
+    return match[0].value
+
+
+def main():
+    args = arg_parser()
+    if args.name_of_def == 'csv':
+        row, column = map(int, args.value)
+        print(csv_reader(row, column))
+    else:
+        jpath = args.value[0]
+        print(json_reader(jpath))
+
+
+if __name__ == "__main__":
+    main()
